a98584224cb599815f1b071380e5b56f20659511
Nitesh Gupta <anujniteshgupta2006@gmail.com>
2025-01-25 20:41:50 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -2,10 +2,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const submitButton = document.querySelector('button[type="submit"]');
     submitButton.addEventListener('click', (event) => {
         event.preventDefault();
-        alert('Form submitted');
+        const username = document.querySelector('input[name="username"]').value;
+        const password = document.querySelector('input[name="password"]').value;
         
+        if (username === '' || password === '') {
+            alert('Please fill in all fields');
+            return;
+        }
+        
+        alert('Login Done');
         document.querySelector('input[name="username"]').value = '';
         document.querySelector('input[name="password"]').value = '';
         document.querySelector('input[name="remember_me"]').checked = false;
+        
+        window.location.href = 'after-submision.html';
     });
 });
